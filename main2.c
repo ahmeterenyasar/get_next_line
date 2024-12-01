162d5a76ac826a49f0c8a72f82c0520fb4e42ef0
Eren Yaşar <101502124+ahmeterenyasar@users.noreply.github.com>
2024-12-01 16:09:16 +0300

    Add files via upload

--- main2.c
+++ main2.c
@@ -1,46 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main2.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayasar <ayasar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 15:50:47 by ayasar            #+#    #+#             */
+/*   Updated: 2024/12/01 15:59:29 by ayasar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <fcntl.h>
 #include <stdio.h>
 #include "get_next_line.h"
 
-int main(void)
-{
-    int fd;
-    char *line;
+// int main(void)
+// {
+//     int fd;
+//     char *line;
 
-    fd = open("test.txt", O_RDONLY);
+//     fd = open("test.txt", O_RDONLY);
 
-    line = get_next_line(fd);
-    line = get_next_line(fd);
-    line = get_next_line(fd);
+//     line = get_next_line(fd);
+//     line = get_next_line(fd);
+//     line = get_next_line(fd);
     
-    // printf("%s", line);
+//     // printf("%s", line);
     
-    char *line2;
-    line2 = malloc(2);
-    line2[1] = '\0';
-    line2[0] = '\0';
-    line2 = NULL;
+//     char *line2;
+//     line2 = malloc(2);
+//     line2[1] = '\0';
+//     line2[0] = '\0';
+//     line2 = NULL;
 
 
-    printf("Retrieved line: %s\n", line2);
-    free(line);
+//     printf("Retrieved line: %s\n", line2);
+//     free(line);
 
 
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+//     return 0;
+// }
 
 
 
@@ -95,34 +94,35 @@ int main(void)
 //     }
 // }
 
-// int main(void)
-// {
-//     // int fd1;
-//     // int fd2;
-//     // char    *line1;
-//     // char    *line2;
+int main(void)
+{
+    // int fd1;
+    // int fd2;
+    // char    *line1;
+    // char    *line2;
 
    
-//     // fd2 = open("deneme2.txt", O_CREAT | O_RDWR, 0777);
+    // fd2 = open("deneme2.txt", O_CREAT | O_RDWR, 0777);
 
-//     // line1 = get_next_line(fd1);
-//     // line2 = get_next_line(fd2);
-//     // printf("%s", line1);
-//     // printf("%s\n", line2);
+    // line1 = get_next_line(fd1);
+    // line2 = get_next_line(fd2);
+    // printf("%s", line1);
+    // printf("%s\n", line2);
 
-//     int fd1;
-//     int fd2;
-//     char    *line1;
-//     char    *line2;
-//     fd1 = open("deneme1.txt", O_CREAT | O_RDWR, 0777);
-//     fd2 = open("deneme2.txt", O_CREAT | O_RDWR, 0777);
+    int fd1;
+    int fd2;
+    char    *line1;
+    char    *line2;
+    fd1 = open("deneme1.txt", O_CREAT | O_RDWR, 0777);
+    fd2 = open("deneme2.txt", O_CREAT | O_RDWR, 0777);
 
     
-//     while((line1 = get_next_line(fd1)) && (line2 = get_next_line(fd2)))
-//     {
-//         if (line1)
-//             printf("%s", line1);
-//         if (line2)
-//             printf("%s", line2);
-//     }
-// }
+    while((line1 = get_next_line(fd1)) && (line2 = get_next_line(fd2)))
+    {
+        if (line1)
+            printf("%s", line1);
+        if (line2)
+            printf("\n%s", line2);
+        printf("\n");
+    }
+}
