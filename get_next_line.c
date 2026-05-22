0df58bb8dba5752564e2510f8cbad8395b779a2f
Ahmet Eren Yaşar <ahmet.eren.yasar@hotmail.com>
2026-05-22 13:52:44 +0300

    feat: implement toplayici function for reading data and managing linked list nodes

--- get_next_line.c
+++ get_next_line.c
@@ -2,20 +2,44 @@
 
 int toplayici(int fd, t_okuma_chunk_burkay_bey **head)
 {
+    t_okuma_chunk_burkay_bey *niv_node;
 
+    int n;
+    // neden 1 dimi neden 2 değil 3 değil. bu da değişicek.
+    while (1)
+    {
+        niv_node = malloc(sizeof(t_okuma_chunk_burkay_bey));
+        if (!niv_node)
+        {
+            return -1;
+        }
+        niv_node->data = malloc(BUFFER_SIZE);
+        if (!niv_node)
+        {
+            free(niv_node);
+            return -1;
+        }
+
+        //okuyoruz burada:
+        n = read(fd, niv_node->data, BUFFER_SIZE);
+        if (n <= 0)
+        {
+            free(niv_node->data);
+            free(niv_node);
+            return 0;
+            // neden 0? bekle kardeşim öğreticez tm
+        }
+        
+    }
+    
 }
 
 void gnl(int fd)
 {
-    static t_okuma_chunk_burkay_bey *head;
-    head = NULL;
-    // ulan bi üst satır hatalı bakın sonradan null ataması yaparsam hep nulla eşitlenicek tekrar çağırdığım zaman böyle olmaz
-    // düzelticem bu committen sonra
+    static t_okuma_chunk_burkay_bey *head = NULL;
 
     if (fd < 0 || BUFFER_SIZE <= 0)
-    {
         return ;
-    }
 
     // TOPLAMA
     // ÇIKARMA
