03f96ccd3dbbacb1614bc43732385a2e6aa87853
Ahmet Eren Yaşar <ahmet.eren.yasar@hotmail.com>
2026-05-22 13:46:10 +0300

    feat: add initial implementation of toplayici function and create test.txt file

--- get_next_line.c
+++ get_next_line.c
@@ -1,10 +1,16 @@
 #include "get_next_line.h"
-#include <cstddef>
+
+int toplayici(int fd, t_okuma_chunk_burkay_bey **head)
+{
+
+}
 
 void gnl(int fd)
 {
     static t_okuma_chunk_burkay_bey *head;
     head = NULL;
+    // ulan bi üst satır hatalı bakın sonradan null ataması yaparsam hep nulla eşitlenicek tekrar çağırdığım zaman böyle olmaz
+    // düzelticem bu committen sonra
 
     if (fd < 0 || BUFFER_SIZE <= 0)
     {
@@ -15,4 +21,11 @@ void gnl(int fd)
     // ÇIKARMA
     // TEMİZLEME
     // + MAIN yazalım ve test edelim sürekli
+}
+
+int main(int argc, char** argv)
+{
+    int fd = open("test.txt", O_RDONLY);
+    gnl(fd);
+    close(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,14 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 24
+
+
 // chunklar halinde okicaz o yüzden bir strcut oluşturalım linked list yapısı
 typedef struct s_okuma_chunk_burkay_bey
 {
@@ -9,5 +17,7 @@ typedef struct s_okuma_chunk_burkay_bey
     // sonraki node'a geçebilmek için aynı veri türüne ait bir değişkeni de barındırıyor anladınız mı?
     struct okuma_chunk_burkay_bey* next;
 } t_okuma_chunk_burkay_bey;
+
+
 
 #endif
